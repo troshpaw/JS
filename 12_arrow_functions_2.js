e85a8f43750997b0977b3021cb0ba67cb8984609
Pavel <troshpaw@gmail.com>
2023-05-27 20:58:02 +0300

    before errors handing

--- 12_arrow_functions_2.js
+++ 12_arrow_functions_2.js
@@ -26,4 +26,31 @@ console.log(multByFactorArrayFunction(3, 4)) // 12
 console.log(multByFactorArrayFunction(3)) // 15
 
 // Пример 2:
-// 04:57:37 - 05:08:22
+// a) Функция возвращает НЕЯВНОЕ ЗНАЧЕНИЕ (одно из преимуществ стрелочных функций)
+const firstPost = {
+    heading: 'Training',
+    release: 100
+}
+
+const newPost = (post, addDate = Date()) => ({
+    ...post,
+    addDate // то же самое, что и "addDate: addDate"
+})
+// Название параметра "addDate" функции совпадает с названием свойства нового объекта "newPost"
+// Значение параметра "addDate" функции = значению свойства "addDate" нового объекта "newPost"
+// "addDate = Date()" - дефолтный параметр, "Date()" - встроенная в JS функция
+// В данном примере тело функции обернуто в (), а {} содержат тело объекта
+
+const publishedPost = newPost(firstPost)
+console.log(publishedPost)
+
+// b) Аналогичный пример, но С ЯВНЫМ ВОЗВРАТОМ ЗНАЧЕНИЯ ФУНКЦИИ (ОБЪЕКТА)
+const newPostTwo = (post, addDate = Date()) => {
+    return {
+        ...post,
+        addDate
+    }
+}
+
+const publishedPostTwo = newPostTwo(firstPost)
+console.log(publishedPostTwo)
