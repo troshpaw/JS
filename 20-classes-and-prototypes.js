20e09bbf7e275867771dec54b2b09a8cacb0663c
Pavel <troshpaw@gmail.com>
2023-06-05 10:13:30 +0300

    classes and prototypes

--- 20-classes-and-prototypes.js
+++ 20-classes-and-prototypes.js
@@ -1,4 +1,27 @@
 
 // КЛАССЫ И ПРОТОТИПЫ
+/* Классы позволяют создавать прототипы для объектов.
+На основании прототипов создаются экземпляры.
+Экземпляры могут иметь собственные свойства и методы. 
+Экземпляры наследуют свойства и методы прототипов. */
 
-// 08:37
+class Comment {
+    constructor(text) {
+        this.text = text
+        this.votesQty = 0
+    }
+
+    upvote() {
+        this.votesQty += 1
+    }
+}
+
+const firstComment = new Comment('First comment')
+
+console.log(firstComment) // Comment { text: 'First comment', votesQty: 0 }
+// Более подробно рассмотреть структуру объекта можно в консоли браузера.
+
+// Проверка принадлежности конкретного экземпляра классам:
+firstComment instanceof Comment // true
+firstComment instanceof Object // true
+
